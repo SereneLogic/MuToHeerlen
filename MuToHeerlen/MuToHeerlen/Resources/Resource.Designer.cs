470a67721dcade0136773d75d1f49eaa51ee087e
Joshua Pierlo <1535137pierlo@zuyd.nl>
2017-01-11 09:58:11 +0100

    v.0.03

--- MuToHeerlen/MuToHeerlen/Resources/Resource.Designer.cs
+++ MuToHeerlen/MuToHeerlen/Resources/Resource.Designer.cs
@@ -61,10 +61,13 @@ namespace MuToHeerlen
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Layout_Start = 2130903040;
+			public const int Layout_Infoscherm = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int Layout_Start = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Main = 2130903042;
 			
 			static Layout()
 			{
