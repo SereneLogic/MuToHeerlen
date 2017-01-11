feb027a19b506c90258453d865933b8518d80f25
Joshua Pierlo <1535137pierlo@zuyd.nl>
2017-01-11 10:29:06 +0100

    v.0.06 -------------- - Added Class_Mural and its constructor

--- MuToHeerlen/MuToHeerlen/Class_Mural.cs
+++ MuToHeerlen/MuToHeerlen/Class_Mural.cs
@@ -14,11 +14,26 @@ namespace MuToHeerlen
 {
     class Class_Mural
     {
-        public int Id;
-        public string Name;
-        public string Artist;
-        public string Description_NL;
-        public string Description_EN;
-        public string Location;
+        public int Mural_Id;
+        public string Mural_Name;
+        public string Mural_Artist;
+        public int Mural_Year;
+        public string Mural_Location;
+        public string Mural_Description_NL;
+        public string Mural_Description_EN;
+
+
+        //Constructor
+        public Class_Mural(int A, string B, string C, int D, string E, string F, string G)
+        {
+            Mural_Id = A;
+            Mural_Name = B;
+            Mural_Artist = C;
+            Mural_Year = D;
+            Mural_Location = E;
+            Mural_Description_NL = F;
+            Mural_Description_EN = G;
+        }
     }
+
 }
